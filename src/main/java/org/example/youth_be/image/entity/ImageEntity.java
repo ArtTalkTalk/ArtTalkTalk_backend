4eaede65a8c8fdc22b9ac01d1f234401480474db
thisishwan2 <feel000617@gmail.com>
2024-01-27 01:17:11 +0900

    refactor: xxxEntity로 엔티티 클래스 명명

--- src/main/java/org/example/youth_be/image/entity/ImageEntity.java
+++ src/main/java/org/example/youth_be/image/entity/ImageEntity.java
@@ -8,7 +8,7 @@ import lombok.*;
 @AllArgsConstructor
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
-public class Image {
+public class ImageEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
