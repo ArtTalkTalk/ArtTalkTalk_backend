aec0803d803d7af42f638e47a72bcefd298bfe1e
thisishwan2 <feel000617@gmail.com>
2024-01-26 21:23:57 +0900

    feat: 이미지 엔티티 정의

---
+++ src/main/java/org/example/youth_be/image/entity/Image.java
@@ -0,0 +1,26 @@
+package org.example.youth_be.image.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long imageId;
+
+    @Column(name = "image_url", length = 1000, nullable = false)
+    private String imageUrl;
+
+    @Column(name = "image_upload_name", nullable = false)
+    private String imageUploadName;
+
+    @Column(name = "artwork_id", nullable = false)
+    private Long artworkId;
+}
