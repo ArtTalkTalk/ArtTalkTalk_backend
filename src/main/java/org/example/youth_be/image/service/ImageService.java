2cb07996e4a9d5ef008584c2cc2b36ca0be282ac
thisishwan2 <feel000617@gmail.com>
2024-01-26 21:51:21 +0900

    refactor: 불필요한 import 삭제

--- src/main/java/org/example/youth_be/image/service/ImageService.java
+++ src/main/java/org/example/youth_be/image/service/ImageService.java
@@ -1,11 +1,9 @@
 package org.example.youth_be.image.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.youth_be.image.repository.ImageRepository;
 import org.example.youth_be.image.service.dto.UploadImageResponse;
 import org.example.youth_be.s3.service.S3UploadService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
