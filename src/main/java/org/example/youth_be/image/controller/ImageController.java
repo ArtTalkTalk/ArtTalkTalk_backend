f2c3122256adbb07698567fd96963169aae120aa
thisishwan2 <feel000617@gmail.com>
2024-01-26 21:50:49 +0900

    refactor: requestparam으로 수정

--- src/main/java/org/example/youth_be/image/controller/ImageController.java
+++ src/main/java/org/example/youth_be/image/controller/ImageController.java
@@ -3,10 +3,7 @@ package org.example.youth_be.image.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.youth_be.image.service.ImageService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -17,7 +14,7 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadImage(@RequestPart MultipartFile multipartFile) {
+    public ResponseEntity<?> uploadImage(@RequestParam MultipartFile multipartFile) {
         return ResponseEntity.ok(imageService.uploadImage(multipartFile));
     }
 }
