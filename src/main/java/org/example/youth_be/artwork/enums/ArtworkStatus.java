776523d057b58c7b6ebdf72a41cc3e33639672e2
thisishwan2 <feel000617@gmail.com>
2024-01-28 20:02:36 +0900

    fix: enum 수정

--- src/main/java/org/example/youth_be/artwork/enums/ArtworkStatus.java
+++ src/main/java/org/example/youth_be/artwork/enums/ArtworkStatus.java
@@ -1,5 +1,16 @@
 package org.example.youth_be.artwork.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ArtworkStatus {
-    PUBLIC, SELLING, FREE, PRIVATE
+
+    PUBLIC("공개"),
+    SELLING("판매중"),
+    FREE("무료나눔"),
+    PRIVATE("나만 보기");
+
+    private final String description;
 }
