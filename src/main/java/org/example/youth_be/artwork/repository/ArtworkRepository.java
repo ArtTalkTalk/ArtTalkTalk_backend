66fa74e62fd96086bf5dcfce305c0ddb0645134f
thisishwan2 <feel000617@gmail.com>
2024-01-29 18:39:44 +0900

    feat: 커서 기반으로 조회 메서드 추가

--- src/main/java/org/example/youth_be/artwork/repository/ArtworkRepository.java
+++ src/main/java/org/example/youth_be/artwork/repository/ArtworkRepository.java
@@ -1,9 +1,15 @@
 package org.example.youth_be.artwork.repository;
 
 import org.example.youth_be.artwork.domain.ArtworkEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtworkRepository extends JpaRepository<ArtworkEntity, Long> {
+
+    Slice<ArtworkEntity> findAllByUserIdOrderByArtworkIdDesc(Long userId, Pageable pageable);
+
+    Slice<ArtworkEntity> findByUserIdAndArtworkIdLessThanOrderByArtworkIdDesc(Long userId, Long artworkId, Pageable page);
 }
