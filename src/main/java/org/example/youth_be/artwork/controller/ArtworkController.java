64a7af5f9101e05ce984b52737e33fd2886a5932
thisishwan2 <feel000617@gmail.com>
2024-02-01 04:43:09 +0900

    feat: 피드 조회 API 생성

--- src/main/java/org/example/youth_be/artwork/controller/ArtworkController.java
+++ src/main/java/org/example/youth_be/artwork/controller/ArtworkController.java
@@ -3,7 +3,10 @@ package org.example.youth_be.artwork.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.youth_be.artwork.controller.spec.ArtworkSpec;
 import org.example.youth_be.artwork.service.ArtworkService;
+import org.example.youth_be.artwork.service.request.ArtworkPaginationRequest;
 import org.example.youth_be.artwork.service.request.DevArtworkCreateRequest;
+import org.example.youth_be.artwork.service.response.ArtworkResponse;
+import org.example.youth_be.common.PageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,5 +21,11 @@ public class ArtworkController implements ArtworkSpec {
     @ResponseStatus(HttpStatus.CREATED)
     public Long createArtworkForDev(@RequestBody DevArtworkCreateRequest request) {
         return artworkService.createArtworkForDev(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public PageResponse<ArtworkResponse> getArtworks(@RequestParam Long userId, @RequestParam String type, @ModelAttribute ArtworkPaginationRequest request) {
+        return artworkService.getArtworks(userId, type, request);
     }
 }
--- src/main/java/org/example/youth_be/artwork/repository/ArtworkRepositoryCustom.java
+++ src/main/java/org/example/youth_be/artwork/repository/ArtworkRepositoryCustom.java
@@ -1,11 +1,11 @@
 package org.example.youth_be.artwork.repository;
 
-import org.example.youth_be.user.enums.ArtworkType;
 import org.example.youth_be.artwork.service.response.ArtworkResponse;
 import org.springframework.data.domain.Slice;
 
 public interface ArtworkRepositoryCustom {
 
-    Slice<ArtworkResponse> findByUserAndArtworkType(Long userId, Long cursorId, Integer size, ArtworkType type);
+    Slice<ArtworkResponse> findByUserAndArtworkType(Long userId, Long cursorId, Integer size, String type);
 
+    Slice<ArtworkResponse> findByFeedType(Long userId, Long cursorId, Integer size, String type);
 }
