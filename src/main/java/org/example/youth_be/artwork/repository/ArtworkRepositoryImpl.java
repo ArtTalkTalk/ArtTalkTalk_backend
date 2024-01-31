e78d9caa170843a065223537dc1d9b17d0d9fc9f
thisishwan2 <feel000617@gmail.com>
2024-02-01 04:42:45 +0900

    feat: 메인 화면 피드 조회 로직 구현

--- src/main/java/org/example/youth_be/artwork/repository/ArtworkRepositoryImpl.java
+++ src/main/java/org/example/youth_be/artwork/repository/ArtworkRepositoryImpl.java
@@ -6,10 +6,10 @@ import org.example.youth_be.artwork.domain.ArtworkEntity;
 import org.example.youth_be.artwork.domain.QArtworkEntity;
 import org.example.youth_be.artwork.enums.ArtworkStatus;
 import org.example.youth_be.common.exceptions.YouthNotFoundException;
+import org.example.youth_be.follow.domain.QFollowEntity;
 import org.example.youth_be.like.domain.QLikeEntity;
 import org.example.youth_be.user.domain.QUserEntity;
 import org.example.youth_be.user.domain.UserEntity;
-import org.example.youth_be.user.enums.ArtworkType;
 import org.example.youth_be.artwork.service.response.ArtworkResponse;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -28,22 +28,109 @@ public class ArtworkRepositoryImpl implements ArtworkRepositoryCustom {
     QArtworkEntity artwork = QArtworkEntity.artworkEntity;
     QUserEntity user = QUserEntity.userEntity;
     QLikeEntity like = QLikeEntity.likeEntity;
+    QFollowEntity follow = QFollowEntity.followEntity;
 
     @Override
-    public Slice<ArtworkResponse> findByUserAndArtworkType(Long userId, Long cursorId, Integer size, ArtworkType type) {
+    public Slice<ArtworkResponse> findByUserAndArtworkType(Long userId, Long cursorId, Integer size, String type) {
         switch (type) {
-            case ALL:
+            case "ALL":
                 return findAllByCondition(userId, cursorId, size);
-            case SELLING:
+            case "SELLING":
                 return findSellingsByCondition(userId, cursorId, size);
-            case COLLECTION:
+            case "COLLECTION":
                 return findCollectionByCondition(userId, cursorId, size);
             default:
                 throw new YouthNotFoundException("작품을 찾을 수 없습니다.", null);
         }
     }
 
-    private Slice<ArtworkResponse> findAllByCondition(Long userId, Long cursorId, Integer size) {
+    @Override
+    public Slice<ArtworkResponse> findByFeedType(Long userId, Long cursorId, Integer size, String type) {
+        switch (type) {
+            case "ALL":
+                return findAllByCondition(cursorId, size);
+            case "FOLLOWING":
+                return findFollowingByCondition(userId, cursorId, size);
+            default:
+                throw new YouthNotFoundException("작품을 찾을 수 없습니다.", null);
+        }
+    }
+
+    private Slice<ArtworkResponse> findAllByCondition(Long cursorId, Integer size) {
+        // 커서 ID를 기준으로 Artwork 조회
+        List<ArtworkEntity> artworks = jpaQueryFactory
+                .selectFrom(artwork)
+                        .where(artwork.artworkId.lt(cursorId))
+                .orderBy(artwork.artworkId.desc())
+                .limit(size + 1)
+                .fetch();
+
+        // UserArtworkResponse로 변환
+        List<ArtworkResponse> responses = artworks.stream().limit(size).map(a -> {
+            // UserEntity 조회
+            UserEntity artist = jpaQueryFactory
+                    .selectFrom(user)
+                    .where(user.userId.eq(a.getUserId()))
+                    .fetchOne();
+
+            return ArtworkResponse.of(a.getArtworkId(), a.getTitle(), a.getDescription(), a.getArtworkStatus(),
+                    a.getViewCount(), a.getLikeCount(), a.getCommentCount(), a.getThumbnailImageUrl(), a.getUserId(),
+                    artist.getNickname(), artist.getProfileImageUrl(), a.getCreatedAt(), a.getUpdatedAt());
+        }).collect(Collectors.toList());
+
+
+        boolean hasNext = artworks.size() > size;
+
+        // Pageable 객체 생성
+        Pageable pageable = PageRequest.of(0, size);
+
+        // SliceImpl 객체로 반환
+        return new SliceImpl<>(responses, pageable, hasNext);
+    }
+
+    private Slice<ArtworkResponse> findFollowingByCondition(Long userId, Long cursorId, Integer size) {
+        // 사용자가 팔로우한 사용자 ID 목록 조회
+        List<Long> followingUserIds = jpaQueryFactory
+                .select(follow.receiverId)
+                .from(follow)
+                .where(follow.senderId.eq(userId))
+                .fetch();
+
+        // 사용자가 팔로우한 사용자의 Artwork 조회
+        List<ArtworkEntity> artworks = jpaQueryFactory
+                .selectFrom(artwork)
+                .where(artwork.userId.in(followingUserIds)
+                        .and(artwork.artworkId.lt(cursorId))) // 커서 ID 조건
+                .orderBy(artwork.artworkId.desc())
+                .limit(size + 1) // hasNext 판단을 위해 size + 1개 조회
+                .fetch();
+
+        // UserArtworkResponse로 변환
+        List<ArtworkResponse> responses = artworks.stream().limit(size).map(a -> {
+            // UserEntity 조회
+            UserEntity artist = jpaQueryFactory
+                    .selectFrom(user)
+                    .where(user.userId.eq(a.getUserId()))
+                    .fetchOne();
+
+            return ArtworkResponse.of(a.getArtworkId(), a.getTitle(), a.getDescription(), a.getArtworkStatus(),
+                    a.getViewCount(), a.getLikeCount(), a.getCommentCount(), a.getThumbnailImageUrl(), a.getUserId(),
+                    artist.getNickname(), artist.getProfileImageUrl(), a.getCreatedAt(), a.getUpdatedAt());
+        }).collect(Collectors.toList());
+
+
+        boolean hasNext = artworks.size() > size;
+
+        // Pageable 객체 생성
+        Pageable pageable = PageRequest.of(0, size);
+
+        // SliceImpl 객체로 반환
+        return new SliceImpl<>(responses, pageable, hasNext);
+    }
+
+
+
+        private Slice<ArtworkResponse> findAllByCondition(Long userId, Long cursorId, Integer size) {
 
         UserEntity artist = jpaQueryFactory
                 .selectFrom(user)
