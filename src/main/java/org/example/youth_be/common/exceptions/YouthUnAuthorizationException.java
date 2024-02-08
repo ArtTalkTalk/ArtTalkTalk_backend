d1d83c464b52e1ad7096b8f19d0665d468ca5cc6
thisishwan2 <feel000617@gmail.com>
2024-02-09 00:54:05 +0900

    fix: 예외 추상화

--- src/main/java/org/example/youth_be/common/exceptions/YouthUnAuthorizationException.java
+++ src/main/java/org/example/youth_be/common/exceptions/YouthUnAuthorizationException.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class YouthAccessTokenExpireException extends YouthException {
-    public YouthAccessTokenExpireException(String clientMessage, String debugMessage) {
+public class YouthUnAuthorizationException extends YouthException {
+    public YouthUnAuthorizationException(String clientMessage, String debugMessage) {
         super(HttpStatus.UNAUTHORIZED, clientMessage, debugMessage);
     }
 }
