55e16760181c2f2a854564f25f9a4a3be8aefe80
thisishwan2 <feel000617@gmail.com>
2024-02-09 22:14:53 +0900

    fix: 서블릿 중복 문제 해결 및 h2 접근 허용

--- src/main/java/org/example/youth_be/common/security/SecurityConfig.java
+++ src/main/java/org/example/youth_be/common/security/SecurityConfig.java
@@ -4,20 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.example.youth_be.common.jwt.JwtAccessDeniedHandler;
 import org.example.youth_be.common.jwt.JwtAuthenticationFilter;
 import org.example.youth_be.user.enums.UserRoleEnum;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,6 +31,9 @@ public class SecurityConfig {
 
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private static final String[] PATTERNS = {
+            "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**"
+    };
 
 
     /*
@@ -48,19 +56,23 @@ public class SecurityConfig {
     }
 
     @Bean
-    protected SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+    protected SecurityFilterChain filterChain(final HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) //csrf 비활성
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .formLogin(AbstractHttpConfigurer::disable) //폼 로그인 비활성
                 .httpBasic(AbstractHttpConfigurer::disable) //HTTP 기본인증 비활성
                 .sessionManagement((sessionManagement) ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/artworks/**", "/users//{userId}", "/users/{userId}/artworks").permitAll() // 회원이 아니어도 접근 가능
-                        .requestMatchers(HttpMethod.POST, "/추가회원가입").hasRole(String.valueOf(UserRoleEnum.ASSOCIATE)) // 준회원은 추가 회원가입 가능
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers(Stream.of(PATTERNS).map(AntPathRequestMatcher::antMatcher).toArray(AntPathRequestMatcher[]::new)).permitAll()
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/artworks/**")).permitAll()
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/users/{userId}")).permitAll()
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/users/{userId}/artworks")).permitAll() // 회원이 아니어도 접근 가능
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/추가회원가입")).hasRole(String.valueOf(UserRoleEnum.ASSOCIATE)) // 준회원은 추가 회원가입 가능
                         .anyRequest().hasRole(String.valueOf(UserRoleEnum.REGULAR)) // 정회원은 모든 api 접근 가능
                 )
                 .exceptionHandling(exceptionHandling ->
@@ -71,3 +83,17 @@ public class SecurityConfig {
         return http.build();
     }
 }
+
+/*
+.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers(Stream.of(PATTERNS).map(AntPathRequestMatcher::antMatcher).toArray(AntPathRequestMatcher[]::new)).permitAll()
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/artworks/**"),
+                                new AntPathRequestMatcher("/users/{userId}"),
+                                new AntPathRequestMatcher("/users/{userId}/artworks")).permitAll() // 회원이 아니어도 접근 가능
+                        .requestMatchers(new AntPathRequestMatcher("/추가회원가입")).hasRole(String.valueOf(UserRoleEnum.ASSOCIATE)) // 준회원은 추가 회원가입 가능
+                        .anyRequest().hasRole(String.valueOf(UserRoleEnum.REGULAR)) // 정회원은 모든 api 접근 가능
+                )
+ */
+
