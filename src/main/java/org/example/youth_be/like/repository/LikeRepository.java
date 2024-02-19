bcd59fe80721f925d7cc36edbd11f5335f89c2b4
thisishwan2 <feel000617@gmail.com>
2024-02-20 02:51:53 +0900

    fix: 작가의 total 좋아요 수 증가, 감소 수정

--- src/main/java/org/example/youth_be/like/repository/LikeRepository.java
+++ src/main/java/org/example/youth_be/like/repository/LikeRepository.java
@@ -4,6 +4,10 @@ import org.example.youth_be.like.domain.LikeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
+
+    Optional<Long> findByArtworkIdAndUserId(Long artworkId, Long userId);
 }
