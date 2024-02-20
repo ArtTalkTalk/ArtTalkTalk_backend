683c8588cdbe530f6ecf2405f078c317a3ea25ab
thisishwan2 <feel000617@gmail.com>
2024-02-20 20:57:14 +0900

    hotfix: https 적용에 따른 swagger 수정

--- src/main/java/org/example/youth_be/common/SwaggerConfig.java
+++ src/main/java/org/example/youth_be/common/SwaggerConfig.java
@@ -2,6 +2,7 @@ package org.example.youth_be.common;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -11,7 +12,7 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
+@OpenAPIDefinition(servers = {@Server(url = "https://www.art-talktalk.store", description = "서버 도메인")},
         info = @Info(title = "청춘예찬 App",
                 description = "청춘예찬 api명세",
                 version = "v1"))
