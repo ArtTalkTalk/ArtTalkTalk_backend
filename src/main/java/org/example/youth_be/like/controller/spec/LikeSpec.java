35b485b42daf1b408fa5f504b9a7675759f25695
thisishwan2 <112103038+thisishwan2@users.noreply.github.com>
2024-02-22 16:50:59 +0900

    fix: like 등록시 응답에 id 추가 (#55)

--- src/main/java/org/example/youth_be/like/controller/spec/LikeSpec.java
+++ src/main/java/org/example/youth_be/like/controller/spec/LikeSpec.java
@@ -4,12 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.youth_be.common.ApiTags;
 import org.example.youth_be.common.jwt.TokenClaim;
+import org.example.youth_be.like.service.CreateLikeResponse;
 
 @Tag(name = ApiTags.LIKE)
 public interface LikeSpec {
 
     @Operation(description = "작품 좋아요 생성 API입니다.")
-    void createArtworkLike(Long artworkId, TokenClaim tokenClaim);
+    CreateLikeResponse createArtworkLike(Long artworkId, TokenClaim tokenClaim);
 
     @Operation(description = "작품 좋아요 삭제 API입니다.")
     void deleteArtworkLike(Long artworkId, Long likeId, TokenClaim tokenClaim);
---
+++ src/main/java/org/example/youth_be/like/service/CreateLikeResponse.java
@@ -0,0 +1,12 @@
+package org.example.youth_be.like.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class CreateLikeResponse {
+    private Long likeId;
+}
