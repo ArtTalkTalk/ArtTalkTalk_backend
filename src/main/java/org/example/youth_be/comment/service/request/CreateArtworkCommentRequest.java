61650d8132b28da821edfd0d989729c39ceeebd6
arsgsg1 <arsgsg1@gmail.com>
2024-02-23 19:03:19 +0900

    기본 생성자 추가

--- src/main/java/org/example/youth_be/comment/service/request/CreateArtworkCommentRequest.java
+++ src/main/java/org/example/youth_be/comment/service/request/CreateArtworkCommentRequest.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateArtworkCommentRequest {
     @Schema(description = "댓글 내용, 글자 제한 없음")
     @NotNull
