c6357b8f902b2d957fe71a58ac98381b129868b9
thisishwan2 <feel000617@gmail.com>
2024-02-23 16:37:43 +0900

    hotfix: redis key null 에러처리

--- src/main/java/org/example/youth_be/common/redis/TokenRedisRepository.java
+++ src/main/java/org/example/youth_be/common/redis/TokenRedisRepository.java
@@ -3,6 +3,7 @@ package org.example.youth_be.common.redis;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.youth_be.common.exceptions.YouthBadRequestException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,11 @@ public class TokenRedisRepository implements TokenRepository {
     @Override
     public Boolean hasBlackList(@NotNull String key) {
         ValueOperations<String, Object> values = redisTemplate.opsForValue();
+
+        if (key == null){
+            throw new YouthBadRequestException("토큰이 존재하지 않습니다.", null);
+        }
+
         if (values.get(key) != null) { // 블랙리스트로 되어 있는 경우 true
             return true;
         }
